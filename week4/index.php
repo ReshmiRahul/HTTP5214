36286e5c0dabaa9598ec226466ec5f6573a781ed
ReshmiRahul <reshmihl94@gmail.com>
2024-09-26 23:57:21 -0400

    Add modified week 4 index.php file

--- week4/index.php
+++ week4/index.php
@@ -1,14 +1,15 @@
 <?php
 // Function to retrieve user data from the API
-function fetchUsers() {
-    $apiEndpoint = "https://jsonplaceholder.typicode.com/users";
-    $response = file_get_contents($apiEndpoint);
-    return json_decode($response, true);
+function getUsers() {
+    $url = "https://jsonplaceholder.typicode.com/users";
+    $data = file_get_contents($url);
+    return json_decode($data, true);
 }
 
 // Fetch user data
-$users = fetchUsers();
+$users = getUsers();
 
+// Include Bootstrap stylesheet
 echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
@@ -16,7 +17,7 @@ echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/
 <div class="container my-5">
     <div class="row">';
 
-// Iterate over users array 
+// Iterate over users array and output each user card
 foreach ($users as $user) {
     $userAddress = $user['address'];
     echo '<div class="col-md-4 mb-4">
@@ -37,3 +38,4 @@ echo '    </div>
 </body>
 </html>';
 ?>
+
